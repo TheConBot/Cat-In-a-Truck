4d07799079a9f913179718f9f1a08be7a9061e8d
Connor Botts <connor.botts@gmail.com>
2017-07-03 13:43:52 -0400

    Created Recipie ScriptableObject

---
+++ Assets/Scripts/ScriptableObjects/Recipie.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu()]
+public class Recipie : ScriptableObject
+{
+    public string displayName;
+    [Range(1, 5)]
+    public int occuranceFrequency;
+    //Change to Ingrediant Object once it's made
+    private const int INGREDIANT_MAX_AMMOUNT = 4;
+    public UnityEngine.Object[] ingrediants = new UnityEngine.Object[INGREDIANT_MAX_AMMOUNT];
+
+    void OnValidate(){
+        if (ingrediants.Length != INGREDIANT_MAX_AMMOUNT) {
+         Debug.LogWarning("Don't change the 'ingrediants' field's array size!");
+         Array.Resize(ref ingrediants, INGREDIANT_MAX_AMMOUNT);
+        }
+    }
+}
