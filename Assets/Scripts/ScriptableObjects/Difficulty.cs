b68c34b68b7a20d270de38a32b38f2c6117a8051
Connor Botts <connor.botts@gmail.com>
2017-07-03 14:23:33 -0400

    Created Game Manager and Difficulty Settings

---
+++ Assets/Scripts/ScriptableObjects/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu()]
+public class Difficulty : ScriptableObject {
+	public float roundTime;
+	public int maxRecipieIngrediants;
+	public bool allowModifications;
+	
+}
