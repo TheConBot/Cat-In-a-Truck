8e3d2f40cc58ef580216e2eef2bc409e85154878
Nathan Wentworth <nwentworth95@gmail.com>
2017-07-07 16:29:42 -0400

    high score gets complete

--- Assets/Scripts/StoreHighScores.cs
+++ Assets/Scripts/StoreHighScores.cs
@@ -2,7 +2,9 @@
 using System.Net;
 using System.IO;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoreHighScores : MonoBehaviour {
 
@@ -10,10 +12,19 @@ public class StoreHighScores : MonoBehaviour {
 
   private List<PlayerInfo> highScores = new List<PlayerInfo>();
 
+  [SerializeField]
+  private InputField nameEntry;
+  [SerializeField]
+  private Text topScoresText;
+
   private void Start() {
-    StoreHighScore("nathan", 420, "normal");
+    // StoreHighScore("nathan", 420, "normal");
 
     GetHighScores();
+  }
+
+  public void HighScoreEntry() {
+    StoreHighScore(nameEntry.text, 123, "normal");
   }
 
   private void StoreHighScore(string name, int score, string text) {
@@ -23,10 +34,10 @@ public class StoreHighScores : MonoBehaviour {
 
   private void GetHighScores() {
     string url = urlBase + HighScoresAPIKey.privateKey +  "/json/10/";
-    GetResponse(url);
+    GetResponse(url, true);
   }
 
-  private void GetResponse(string url) {
+  private void GetResponse(string url, bool json = false) {
     WebRequest wrGETURL;
     wrGETURL = WebRequest.Create(url);
 
@@ -42,22 +53,50 @@ public class StoreHighScores : MonoBehaviour {
       i++;
       sLine = objReader.ReadLine();
       if (sLine != null) {
+        if (json) {
+          sLine = sLine.Substring(36);
+          sLine = sLine.Substring(0, sLine.Length - 4);
+          string[] splitLine = Regex.Split(sLine, @"(?=,{)");
+          foreach (string line in splitLine) {
+            string _line = line;
+            Debug.Log("before: " + _line);
+            if (_line[0] == ',') {
+              _line = _line.Substring(1);
+            }
+            Debug.Log("after: " + _line);
+            var jsonObj = JsonUtility.FromJson<PlayerInfo>(_line);
+            Debug.Log("got " + jsonObj.name + ", " + jsonObj.score);
+            highScores.Add(jsonObj);
+          }
+        }
         Debug.Log(i + " : " + sLine);
-        highScores.Add(JsonUtility.FromJson<PlayerInfo>(sLine));
       }
+    }
+
+    // {"dreamlo":{"leaderboard":{"entry":[{"name":"nathan","score":"420","seconds":"0","text":"normal","date":"7/7/2017 7:18:49 PM"},{"name":"gggggood","score":"123","seconds":"0","text":"normal","date":"7/7/2017 7:53:20 PM"}]}}}
+
+    if (json) {
+      DisplayTopScores();
+    }
+  }
+
+  private void DisplayTopScores() {
+    topScoresText.text = "";
+    foreach (PlayerInfo info in highScores) {
+      topScoresText.text += info.name;
+      topScoresText.text += info.score;
+      topScoresText.text += info.text;
+      topScoresText.text += "\n";
+      Debug.Log(info.name);
+      Debug.Log(info.score);
     }
   }
 
 }
 
+[Serializable]
 public class PlayerInfo {
   public string name;
   public int score;
   public string text;
-
-  public PlayerInfo(string _name, int _score, string _text) {
-    name = _name;
-    score = _score;
-    text = _text;
-  }
 }
