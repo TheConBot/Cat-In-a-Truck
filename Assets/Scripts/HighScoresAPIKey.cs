64cacb0bd0421b3e48548021b9a664dc6882ac00
Nathan Wentworth <nwentworth95@gmail.com>
2017-07-07 13:47:00 -0400

    added highscore base

---
+++ Assets/Scripts/HighScoresAPIKey.cs
@@ -0,0 +1,4 @@
+﻿public static class HighScoresAPIKey {
+  public const string privateKey = "hGQ9Yhc2REKDE-QRvzEXdw39BvscnWAECiWACJAsSjog";
+  public const string publicKey = "595fc53c777fa60d60b92a05";
+}
---
+++ Assets/Scripts/StoreHighScores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using UnityEngine;
+
+public class StoreHighScores : MonoBehaviour {
+
+  private const string urlBase = "http://dreamlo.com/lb/";
+
+  private void Start() {
+    StoreHighScore("nathan", 420);
+  }
+
+  private void StoreHighScore(string name, int score) {
+    string url = urlBase + HighScoresAPIKey.privateKey +  "/add/" + name + "/" + score;
+    WebRequest wrGETURL;
+    wrGETURL = WebRequest.Create(url);
+  }
+
+}
