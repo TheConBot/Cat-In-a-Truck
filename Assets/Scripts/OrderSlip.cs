c8b213ea2220edd8f29dfd9f2ee5420a3fb2cf11
Connor Botts <connor.botts@gmail.com>
2017-07-09 13:21:37 -0400

    Added Order Slip and Moved Sound Files

---
+++ Assets/Scripts/OrderSlip.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class OrderSlip : MonoBehaviour {
+    [HideInInspector]
+    public Recipie recipie;
+    public GameObject foodBoat;
+
+    private void Awake() {
+        foodBoat.SetActive(false);
+    }
+
+    public void TakeOrder() {
+        recipie = Manager.instance.recipies[Random.Range(0, Manager.instance.recipies.Count)];
+        foodBoat.GetComponent<PlateContainer>().Recipie = recipie;
+        foodBoat.gameObject.SetActive(true);
+    }
+}
