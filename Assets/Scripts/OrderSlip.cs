da57b5d1f7bd691a722e49e44bd80111aff7354b
Nathan Wentworth <nwentworth95@gmail.com>
2017-07-09 17:03:00 -0400

    added card timer and visual scripts

--- Assets/Scripts/OrderSlip.cs
+++ Assets/Scripts/OrderSlip.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class OrderSlip : MonoBehaviour {
     [HideInInspector]
     public Recipie recipie;
     public PlateContainer foodBoat;
+
+    private TicketUIView ticketUIView;
 
     private MeshRenderer slipMesh;
     private BoxCollider slipCollider;
@@ -15,11 +18,14 @@ public class OrderSlip : MonoBehaviour {
         foodBoat.gameObject.SetActive(false);
         slipMesh = GetComponent<MeshRenderer>();
         slipCollider = GetComponent<BoxCollider>();
-    }
+        ticketUIView.title = foodBoat.GetComponentInChildren<Text>();
+        ticketUIView.timer = foodBoat.GetComponentInChildren<Image>();
+  }
 
     public void TakeOrder() {
         recipie = Manager.instance.recipies[Random.Range(0, Manager.instance.recipies.Count)];
         foodBoat.Recipie = recipie;
+        ticketUIView.SetTitle(recipie.displayName);
         foodBoat.gameObject.SetActive(true);
         SetActiveSoft(false);
         StartCoroutine(TicketCountdown());
@@ -36,9 +42,11 @@ public class OrderSlip : MonoBehaviour {
     }
 
     public IEnumerator TicketCountdown() {
-        ticketTimer = 30;
+        float initialTime = Manager.instance.DifficultySetting.ticketTime;
+        ticketTimer = initialTime;
         while(ticketTimer > 0) {
             yield return new WaitForSeconds(Time.deltaTime);
+            ticketUIView.SetTimer(ticketTimer, initialTime);
             ticketTimer -= Time.deltaTime;
         }
         foodBoat.RemoveChildrenFromPlate();
