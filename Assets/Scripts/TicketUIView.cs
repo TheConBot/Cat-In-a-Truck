5d15a7bb01f4a859abe62077e38e8c158d6e01b4
Nathan Wentworth <nwentworth95@gmail.com>
2017-07-09 16:45:59 -0400

    updated level prefab with boat ui, and boat ui view script

---
+++ Assets/Scripts/TicketUIView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TicketUIView : MonoBehaviour {
+
+  private Image timer;
+  private Text title;
+
+  public void Start() {
+    timer = GetComponentInChildren<Image>();
+    title = GetComponentInChildren<Text>();
+  }
+
+  public void SetTitle(string text) {
+    title.text = text;
+  }
+
+  public void SetTimer(float timeLeft, float startTime) {
+    timer.fillAmount = (timeLeft / startTime);
+  }
+
+}
