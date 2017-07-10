6948e30b84f18ef19c40a4b008365f640e87b5c9
Nathan Wentworth <nwentworth95@gmail.com>
2017-07-09 23:37:58 -0400

    added function to start round

--- Assets/Scripts/RecipeVisualGenerator.cs
+++ Assets/Scripts/RecipeVisualGenerator.cs
@@ -4,120 +4,126 @@ using UnityEngine.UI;
 
 public class RecipeVisualGenerator : MonoBehaviour {
 
-  [SerializeField]
-  private CanvasGroup mainContainerGroup;
+    [SerializeField]
+    private CanvasGroup mainContainerGroup;
 
-  [SerializeField]
-  private Text titleText;
+    [SerializeField]
+    private Text titleText;
 
-  [SerializeField]
-  private Text pageNumber;
+    [SerializeField]
+    private Text pageNumber;
 
-  [SerializeField]
-  private GameObject stepPanelContainer;
+    [SerializeField]
+    private GameObject stepPanelContainer;
 
-  [SerializeField]
-  private List<GameObject> steps;
+    [SerializeField]
+    private List<GameObject> steps;
 
-  [SerializeField]
-  private List<Sprite> solidSprites;
+    [SerializeField]
+    private List<Sprite> solidSprites;
 
-  [SerializeField]
-  private List<Sprite> liquidSprites;
+    [SerializeField]
+    private List<Sprite> liquidSprites;
 
-  [SerializeField]
-  private List<Sprite> cookstateSprites;
+    [SerializeField]
+    private List<Sprite> cookstateSprites;
 
-  [SerializeField]
-  private Sprite cutSprite;
+    [SerializeField]
+    private Sprite cutSprite;
 
-  private int bookIndex = 0;
+    private int bookIndex = 0;
 
-  private void Start() {
-    steps = StoreSteps();
+    private void Start() {
+        steps = StoreSteps();
 
-    ToggleMenu(1);
-    IncrementRecipe(0);
-  }
-
-  private List<GameObject> StoreSteps() {
-    List<GameObject> _steps = new List<GameObject>();
-    for (int i = 0; i < stepPanelContainer.transform.childCount; i++) {
-      GameObject step = stepPanelContainer.transform.GetChild(i).gameObject;
-      step.SetActive(false);
-      _steps.Add(step);
-    }
-    return _steps;
-  }
-
-  private List<Image> GetImages(GameObject container) {
-    List<Image> _images = new List<Image>();
-    for (int i = 0; i < container.transform.childCount; i++) {
-      Image img = container.transform.GetChild(i).GetComponent<Image>();
-      img.gameObject.SetActive(false);
-      _images.Add(img);
+        ToggleMenu(1);
+        IncrementRecipe(0);
     }
 
-    return _images;
-  }
-
-  public void IncrementRecipe(int index) {
-    bookIndex += index;
-    int count = Manager.instance.recipies.Count;
-    if (bookIndex < 0) {
-      bookIndex = count - 1;
-    } else if (bookIndex >= count) {
-      bookIndex = 0;
+    private List<GameObject> StoreSteps() {
+        List<GameObject> _steps = new List<GameObject>();
+        for (int i = 0; i < stepPanelContainer.transform.childCount; i++) {
+            GameObject step = stepPanelContainer.transform.GetChild(i).gameObject;
+            step.SetActive(false);
+            _steps.Add(step);
+        }
+        return _steps;
     }
 
-    pageNumber.text = (bookIndex + 1) + "/" + count;
-    CreateRecipeVisual(Manager.instance.recipies[bookIndex]);
-  }
-
-  public void ToggleMenu(int open = -1) {
-    if (open == -1) {
-      open = (int)mainContainerGroup.alpha;
-    }
-    mainContainerGroup.alpha = open;
-    mainContainerGroup.interactable = (open == 1);
-    mainContainerGroup.blocksRaycasts = (open == 1);
-  }
-
-  public void CreateRecipeVisual(Recipie r) {
-    titleText.text = r.displayName;
-
-    for (int i = 0; i < r.ingredients.Length; i++) {
-      List<Image> images = GetImages(steps[i]);
-
-      int imageIndex = 0;
-
-      if (r.ingredients[i] is SolidRecipieIngredient) {
-        SolidRecipieIngredient sri = r.ingredients[i] as SolidRecipieIngredient;
-        images[imageIndex].sprite = solidSprites[(int)sri.GetSolidType];
-        images[imageIndex].gameObject.SetActive(true);
-        imageIndex++;
-
-        if (sri.IsCut) {
-          images[imageIndex].sprite = cutSprite;
-          images[imageIndex].gameObject.SetActive(true);
-          imageIndex++;
+    private List<Image> GetImages(GameObject container) {
+        List<Image> _images = new List<Image>();
+        for (int i = 0; i < container.transform.childCount; i++) {
+            Image img = container.transform.GetChild(i).GetComponent<Image>();
+            img.gameObject.SetActive(false);
+            _images.Add(img);
         }
 
-        if ((int)sri.GetCookState > 0) {
-          images[imageIndex].sprite = cookstateSprites[(int)sri.GetCookState];
-          images[imageIndex].gameObject.SetActive(true);
+        return _images;
+    }
+
+    public void IncrementRecipe(int index) {
+        bookIndex += index;
+        int count = Manager.instance.recipies.Count;
+        if (bookIndex < 0) {
+            bookIndex = count - 1;
+        }
+        else if (bookIndex >= count) {
+            bookIndex = 0;
         }
 
-      } else if (r.ingredients[i] is LiquidRecipieIngredient) {
-        LiquidRecipieIngredient lri = r.ingredients[i] as LiquidRecipieIngredient;
-        Debug.Log(lri.GetLiquidType);
-        images[imageIndex].sprite = liquidSprites[(int)lri.GetLiquidType];
-        images[imageIndex].gameObject.SetActive(true);
-
-      }
-
-      steps[i].SetActive(true);
+        pageNumber.text = (bookIndex + 1) + "/" + count;
+        CreateRecipeVisual(Manager.instance.recipies[bookIndex]);
     }
-  }
+
+    public void ToggleMenu(int open = -1) {
+        if (open == -1) {
+            open = (int)mainContainerGroup.alpha;
+        }
+        mainContainerGroup.alpha = open;
+        mainContainerGroup.interactable = (open == 1);
+        mainContainerGroup.blocksRaycasts = (open == 1);
+
+        if (open == 0 && Manager.instance.CurrentState != Manager.GameState.Playing) {
+            Manager.instance.StartRound();
+        }
+    }
+
+    public void CreateRecipeVisual(Recipie r) {
+        titleText.text = r.displayName;
+
+        for (int i = 0; i < r.ingredients.Length; i++) {
+            List<Image> images = GetImages(steps[i]);
+
+            int imageIndex = 0;
+
+            if (r.ingredients[i] is SolidRecipieIngredient) {
+                SolidRecipieIngredient sri = r.ingredients[i] as SolidRecipieIngredient;
+                images[imageIndex].sprite = solidSprites[(int)sri.GetSolidType];
+                images[imageIndex].gameObject.SetActive(true);
+                imageIndex++;
+
+                if (sri.IsCut) {
+                    images[imageIndex].sprite = cutSprite;
+                    images[imageIndex].gameObject.SetActive(true);
+                    imageIndex++;
+                }
+
+                if ((int)sri.GetCookState > 0) {
+                    images[imageIndex].sprite = cookstateSprites[(int)sri.GetCookState];
+                    images[imageIndex].gameObject.SetActive(true);
+                }
+
+            }
+            else if (r.ingredients[i] is LiquidRecipieIngredient) {
+                LiquidRecipieIngredient lri = r.ingredients[i] as LiquidRecipieIngredient;
+                Debug.Log(lri.GetLiquidType);
+                images[imageIndex].sprite = liquidSprites[(int)lri.GetLiquidType];
+                images[imageIndex].gameObject.SetActive(true);
+
+            }
+
+            steps[i].SetActive(true);
+        }
+    }
 
 }
