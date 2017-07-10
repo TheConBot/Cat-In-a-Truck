f3bba2d6ebe43c6a957300d8a4be2bab7a302948
Nathan Wentworth <nwentworth95@gmail.com>
2017-07-09 22:13:39 -0400

    added in-game ui for containers

--- Assets/Scripts/Container/CookingContainer.cs
+++ Assets/Scripts/Container/CookingContainer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CookingContainer : Container {
 
@@ -12,8 +13,18 @@ public class CookingContainer : Container {
     private CookOptions cookOption;
     [SerializeField]
     private bool collectTimeEnabled;
+    [SerializeField]
+    private Text statusText;
+    [SerializeField]
+    private Image collectTimer;
+    [SerializeField]
+    private CanvasGroup mainGroup;
     [Range(1, 60)]
     public float cookTime = 10, collectTime = 5;
+
+    private void Start() {
+        ToggleStatus(0);
+    }
 
     override public void AddToContainer(Ingredient ingredient) {
         base.AddToContainer(ingredient);
@@ -22,6 +33,7 @@ public class CookingContainer : Container {
 
     override public Ingredient TakeFromContainer() {
         StopAllCoroutines();
+        ToggleStatus(0);
         return base.TakeFromContainer();
     }
 
@@ -38,26 +50,69 @@ public class CookingContainer : Container {
     }
 
     private IEnumerator CookFood(SolidIngredient ingredient) {
+        ToggleStatus(1);
         Debug.Log("Ingredient Added: " + ingredient.DisplayName + ", Cook Time: " + cookTime + ", Cook Method: " + cookOption + ", Can Ruin: " + collectTimeEnabled);
-        yield return new WaitForSeconds(cookTime);
+
+
+        SetStatus("Cooking");
+        float _cookTime = cookTime;
+        while (_cookTime > 0) {
+            yield return new WaitForSeconds(Time.deltaTime);
+            SetTimer(_cookTime, cookTime);
+            _cookTime -= Time.deltaTime;
+        }
+
         switch (cookOption) {
             case CookOptions.Cut:
                 ingredient.Cut();
+                SetStatus("Cut!");
                 break;
             case CookOptions.Fry:
                 ingredient.Fry();
+                SetStatus("Fried!");
                 break;
             case CookOptions.Steam:
                 ingredient.Steam();
+                SetStatus("Steamed!");
                 break;
         }
+
         Debug.Log("Cooking Complete! " + ingredient.DisplayName + " is " + ingredient.GetCookState + " and " + (ingredient.IsCut ? "Cut" : "Not Cut") + ". Collect your " + ingredient.DisplayName + ".");
+
+
         if (collectTimeEnabled) {
             Debug.Log("Collect Time: " + collectTime);
-            yield return new WaitForSeconds(collectTime);
+            float _collectTime = collectTime;
+            while (_collectTime > 0) {
+                yield return new WaitForSeconds(Time.deltaTime);
+                SetTimer(_collectTime, collectTime);
+                _collectTime -= Time.deltaTime;
+            }
+
+            SetStatus("Ruined");
             ingredient.Ruin();
             Debug.Log("Oh No! " + ingredient.DisplayName + " is " + ingredient.GetCookState + ".");
         }
         yield return null;
     }
+
+    public void SetTimer(float timeLeft, float startTime) {
+        float fillAmount = (timeLeft / startTime);
+        collectTimer.fillAmount = fillAmount;
+    }
+
+    public void SetStatus(string text) {
+        statusText.text = text;
+    }
+
+    public void ToggleStatus(int open = -1) {
+        if (open == -1) {
+            open = (int)mainGroup.alpha;
+        }
+        mainGroup.alpha = open;
+        mainGroup.interactable = (open == 1);
+        mainGroup.blocksRaycasts = (open == 1);
+    }
+
+
 }
